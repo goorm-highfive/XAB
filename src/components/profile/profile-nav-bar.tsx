5c645c5fd34812c816a0fd9359451f0fe4efbe3d
조성원 <tjddnjsjo@kakao.com>
2024-12-19 01:34:36 +0900

    feat: restyle navbar

--- src/components/profile/profile-nav-bar.tsx
+++ src/components/profile/profile-nav-bar.tsx
@@ -1,25 +1,29 @@
 import { Bell, User } from 'lucide-react'
 import Image from 'next/image'
+
 import { Button } from '~/components/ui/button'
+import Logo from '~/assets/svgs/logo.svg'
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 flex items-center justify-between bg-white p-4 shadow-sm">
-      {/* Logo */}
-      <div className="flex items-center gap-2">
-        <Image src="/xAB.svg" width={32} height={32} alt="Website Logo" />
-      </div>
+    <nav className="sticky top-0 z-50 bg-white shadow-sm">
+      <div className="mx-auto flex max-w-screen-2xl items-center justify-between px-6 py-4">
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <Image src={Logo} alt="Website Logo" className="size-8" />
+        </div>
 
-      {/* Navbar 메뉴 */}
-      <div className="flex items-center gap-4">
-        <Button variant="ghost" className="flex items-center gap-2">
-          <User className="h-5 w-5" />
-          Profile
-        </Button>
-        <Button variant="ghost" className="flex items-center gap-2">
-          <Bell className="h-5 w-5" />
-          Notifications
-        </Button>
+        {/* Navbar 메뉴 */}
+        <div className="flex items-center gap-4">
+          <Button variant="ghost" className="flex items-center gap-2">
+            <User className="h-5 w-5" />
+            Profile
+          </Button>
+          <Button variant="ghost" className="flex items-center gap-2">
+            <Bell className="h-5 w-5" />
+            Notifications
+          </Button>
+        </div>
       </div>
     </nav>
   )
