1f96700b95f32cef77a09f6ce7df9ed406b0a55b
E0min <59902527+E0min@users.noreply.github.com>
2024-12-19 15:35:02 +0900

    fix: common에 있는 SiteHeader이용

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,10 +1,10 @@
 import SettingsMenu from '~/components/setting/setting-menu'
-import { Navbar } from '~/components/common/nav-bar'
+import { SiteHeader } from '~/components/common/site-header'
 import { ProfileCard } from '~/components/setting/setting-profile-card'
 export default function SettingsPage() {
   return (
     <div>
-      <Navbar />
+      <SiteHeader />
       <main className="flex min-h-screen items-center justify-center bg-gray-50">
         <div className="w-full max-w-md rounded-lg bg-white p-6 shadow">
           <section className="p-0 pb-6">
